2c74a65fa7e0ac553af0709b9c7aa82e43d6e0f4
ocikhart <119431099+ocikhart@users.noreply.github.com>
2022-12-05 13:13:45 +0100

    Update __init__.py
    Ahoj instead hello

--- myHttpTrigger/__init__.py
+++ myHttpTrigger/__init__.py
@@ -5,8 +5,6 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     
-    choices = 1
-
     name = req.params.get('name')
     if not name:
         try:
@@ -17,7 +15,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Ahoj, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
